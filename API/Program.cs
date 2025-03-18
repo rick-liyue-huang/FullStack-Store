6fa66f33345dba00bf3a572512f667a27ad1f7d5
Rick Huang <rick.liyue.huang@gmail.com>
2025-03-18 16:22:11 +1100

    feat: update the program.cs and add ef to get the sqlite database

--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,30 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi(); // swagger support
+
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // redirect http to https
 
-app.UseAuthorization();
+// app.UseAuthorization(); // enable authorization
 
 app.MapControllers();
 
